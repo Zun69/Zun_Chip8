a4615afdc72609eeb571d5aa4c48ed6321144de8
Zun69 <xinzunlee46@hotmail.com>
2023-03-03 02:55:10 +1100

    Created header file that cointains declaration of classes, functions, etc. Implemented load rom into memory function and loading fontset into memory in the constructor.

---
+++ Zun_Chip8/Chip8.hpp
@@ -0,0 +1,84 @@
+//contains declaration of classes, functions and variables
+
+#pragma once
+
+#include <cstdint>
+#include <random>
+
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+
+// Register is a dedicated location on a cpu for storage. Any operation that a CPU does must be done within its registers.
+    // Operations involve loading data from memory into registers, operating on those registers, and then storing the result back into memory.
+    // Chip 8 has 16 8-bit registers, labeled V0 to VF (hex) Register VF is a bit special. It’s used as a flag to hold information about the result of operations. 
+    // Each register is able to hold any value from 0x00 to 0xFF.
+
+
+    // Chip-8 has 4096 bytes of memory, address space is from 0x000 to 0xFFF
+    // 0x000-0x1FF originally reserved for the chip 8 interpreter, but this emulator will never write or read from this area
+    // 0x050-0x0A0 is storage space for the 16 built in characters (0 through F) which we will need to manually put into our memory because ROMs will be looking for those characters
+    // 0x200-0xFFF stores instructions from the rom, anything left after the ROM's space is free to use
+
+
+    // 16-bit index register is a special register used to store memory addresses for use in operations. It's a a 16 bit register because last memory address (0xFFF) is too big for 8 bit register
+
+    // 16-bit program counter is a special register that holds the address of the next instruction to execute, 16 bit because it needs to store (0xFFF memory address)
+    // Program counter is a CPU's way of keeping track of which instruction to execute next
+    // Opcode is 2 byes but memory is addressed as a single byte, so when we fetch an instrucion from memory we need to fetch a byte from PC and a byte from PC+1 and connect them into a single value
+    // Then increment PC by 2 because we have to increment the PC before executing any instructions because some instructions will manipulate the PC to control program flow.
+
+
+    // 16 level stack, a stack is a way for a CPU to keep track of the order of execution when it calls into functions.
+    // There is an instruction (CALL) that will cause the CPU to begin executing instructions in different region of the program.
+    // When the program reaches another instruction (RET), it must be able to go back to where it was when it hit the (CALL) instruction.
+    // The stack holds the PC value when the CALL instruction was executed, and the RETURN statement pulls that address from the stack and puts it back into the PC so the CPU will execute it on the next cycle.
+    // Means the PC will return to address before CALL instruction was executed.
+    // Chip-8 has 16 levels of stack, meaning it can hold 16 different PC's. Multiple levels allow for one function to call another function and so on, until they all return to the original caller site
+    // Putting a PC onto the stack is called PUSHING and pulling a PC off the stack is called POPPING
+
+
+    // 8-bit stack pointer, similar to how the PC is used to keep track of where in memory the CPU is executing, a SP is used to tell us where in the 16-levels of stack our most recent value was placed (i.e the top)
+    // We only need 8 bits for our stack pointer because the stack will be represented as an array, so the SP can just be an index into that array. We only need sixteen indices then, which a single byte can manage
+    // When we pop a value off the stack, we won’t actually delete it from the array but instead just copy the value and decrement the SP so it “points” to the previous value.
+
+
+    // 8-bit delay timer is a simple timer used for timing, if the timer value is zero it stays zero, if it's loaded with a value it will decrement at a rate of 60hz
+
+
+    // 8-bit sound timer it will decrement at 60hz if non zero, but a single tone will buzz when it's non-zero
+
+
+    //16 input keys, ill take input mapping from website
+
+
+    //64x32 monochrome display memory, memory buffer used for storing the graphics to display, each pixel is either on or off so only two colors can be represented
+
+class Chip8
+{
+public:
+	Chip8(); //declare constructor
+	void LoadROM(char const* filename);
+	void Cycle();
+
+	uint8_t memory[MEMORY_SIZE]{};
+	uint8_t registers[REGISTER_COUNT]{};
+	uint16_t index{};
+	uint16_t pc{};
+	uint8_t delayTimer{};
+	uint8_t soundTimer{};
+	uint16_t stack[STACK_LEVELS]{};
+	uint8_t sp{};
+	uint16_t opcode{};
+
+	uint8_t keypad[KEY_COUNT]{};
+	uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT]{};
+
+    //declare random seed and generating random 8 bit number
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
+
+};
--- Zun_Chip8/Zun_Chip8.cpp
+++ Zun_Chip8/Zun_Chip8.cpp
@@ -2,60 +2,78 @@
 
 #include <iostream>
 #include <fstream>
+#include "Chip8.hpp"
+#include <random>
+#include <chrono>
 
-class Chip8
+const unsigned int FONTSET_SIZE = 80;
+const unsigned int FONTSET_START_ADDRESS = 0x50;
+const unsigned int START_ADDRESS = 0x200;
+
+Chip8::Chip8()
+	: randGen(std::chrono::system_clock::now().time_since_epoch().count()) //seed random generator
 {
-public:
-    // Register is a dedicated location on a cpu for storage. Any operation that a CPU does must be done within its registers.
-    // Operations involve loading data from memory into registers, operating on those registers, and then storing the result back into memory.
-    // Chip 8 has 16 8-bit registers, labeled V0 to VF (hex) Register VF is a bit special. It’s used as a flag to hold information about the result of operations. 
-    // Each register is able to hold any value from 0x00 to 0xFF.
-    uint8_t registers[16]{};
+	//load program counter
+	pc = START_ADDRESS;
 
-    // Chip-8 has 4096 bytes of memory, address space is from 0x000 to 0xFFF
-    // 0x000-0x1FF originally reserved for the chip 8 interpreter, but this emulator will never write or read from this area
-    // 0x050-0x0A0 is storage space for the 16 built in characters (0 through F) which we will need to manually put into our memory because ROMs will be looking for those characters
-    // 0x200-0xFFF stores instructions from the rom, anything left after the ROM's space is free to use
-    uint8_t memory[4096]{};
-    // 16-bit index register is a special register used to store memory addresses for use in operations. It's a a 16 bit register because last memory address (0xFFF) is too big for 8 bit register
-    uint16_t index{};
-    // 16-bit program counter is a special register that holds the address of the next instruction to execute, 16 bit because it needs to store (0xFFF memory address)
-    // Program counter is a CPU's way of keeping track of which instruction to execute next
-    // Opcode is 2 byes but memory is addressed as a single byte, so when we fetch an instrucion from memory we need to fetch a byte from PC and a byte from PC+1 and connect them into a single value
-    // Then increment PC by 2 because we have to increment the PC before executing any instructions because some instructions will manipulate the PC to control program flow.
-    uint16_t pc{};
+	// Load fonts into memory, takes up 80 bytes of memory
+	for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+	{
+		memory[FONTSET_START_ADDRESS + i] = fontset[i];
+	}
 
-    // 16 level stack, a stack is a way for a CPU to keep track of the order of execution when it calls into functions.
-    // There is an instruction (CALL) that will cause the CPU to begin executing instructions in different region of the program.
-    // When the program reaches another instruction (RET), it must be able to go back to where it was when it hit the (CALL) instruction.
-    // The stack holds the PC value when the CALL instruction was executed, and the RETURN statement pulls that address from the stack and puts it back into the PC so the CPU will execute it on the next cycle.
-    // Means the PC will return to address before CALL instruction was executed.
-    // Chip-8 has 16 levels of stack, meaning it can hold 16 different PC's. Multiple levels allow for one function to call another function and so on, until they all return to the original caller site
-    // Putting a PC onto the stack is called PUSHING and pulling a PC off the stack is called POPPING
-    uint16_t stack[16]{};
+	// Initialize RNG
+	randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
+}
 
-    // 8-bit stack pointer, similar to how the PC is used to keep track of where in memory the CPU is executing, a SP is used to tell us where in the 16-levels of stack our most recent value was placed (i.e the top)
-    // We only need 8 bits for our stack pointer because the stack will be represented as an array, so the SP can just be an index into that array. We only need sixteen indices then, which a single byte can manage
-    // When we pop a value off the stack, we won’t actually delete it from the array but instead just copy the value and decrement the SP so it “points” to the previous value.
-    uint8_t sp{};
+//implement declaration of load rom from header file, char const* as the pointer is fixed and cannot change to point to anything else
+void Chip8::LoadROM(char const* filename)
+{
+	//open file in binary mode and sets file pointer to the end of the stream on opening to get size of file
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
 
-    // 8-bit delay timer is a simple timer used for timing, if the timer value is zero it stays zero, if it's loaded with a value it will decrement at a rate of 60hz
-    uint8_t delayTimer{};
+	if (file.is_open())
+	{
+		// Get size of file and allocate a buffer that is the same size as the rom to hold the contents
+		std::streampos size = file.tellg();
+		char* buffer = new char[size];
 
-    // 8-bit sound timer it will decrement at 60hz if non zero, but a single tone will buzz when it's non-zero
-    uint8_t soundTimer{};
+		//go back to beginning of the rom file and fill the buffer
+		file.seekg(0, std::ios::beg); //means offset of 0 from the beginning of file, i.e. the beginning
+		file.read(buffer, size); 
+		file.close();
 
-    //16 input keys, ill take input mapping from website
-    uint8_t keypad[16]{};
+		//load rom (the buffer) into chip 8 memory, starting at 0x200 
+		for (long i = 0; i < size; i++)
+		{
+			memory[START_ADDRESS + i] = buffer[i];
+		}
 
-    //64x32 monochrome display memory, memory buffer used for storing the graphics to display, each pixel is either on or off so only two colors can be represented
-    uint32_t video[64 * 32]{};
-    uint16_t opcode;
+		//free the buffer
+		delete[] buffer;
+	}
+}
+
+uint8_t fontset[FONTSET_SIZE] =
+{
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-int main()
-{
-}
 
 
 
